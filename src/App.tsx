a21949c9df68076e3e55617984703279d1c622cf
stevenacz <scoailazaa@gmail.com>
2021-07-15 22:26:26 -0500

    basic structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+// React
 import React from 'react';
+
+// Pages
+import OrganizationScreen from './pages/organization/OrganizationScreen';
+
+// Styles
+import 'antd/dist/antd.css';
+import GlobalStyle from './theme/globalStyle';
+
+// Components
+import Layout from './components/layout/layout/Layout';
 
 interface Props {}
 
 const App: React.FC<Props> = () => {
   return (
-    <h1>Hello World!</h1>
+    <Layout>
+      <GlobalStyle />
+      <OrganizationScreen />
+    </Layout>
   );
 }
 
